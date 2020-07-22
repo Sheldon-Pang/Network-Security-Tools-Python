11ce76d76a6facdb3caf73a381fb2ea81e3381ba
Sheldon Pang <67815377+Sheldon-Pang@users.noreply.github.com>
2020-07-22 00:38:17 -0700

    MAC changer
    The program changes the MAC address of the interface and verifies the result automatically. 
    use "python MAC_changer --help" for instruction

---
+++ MAC_Changer.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import subprocess
+import optparse
+import re
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
+    #check error
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use -i follow by interface.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new MAC, use -m follow by new MAC.")
+    return options
+
+#change MAC
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+#use Regex to search for MAC address and return the current MAC address
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_search_result:
+        return mac_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
+options = get_arguments()
+
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
+
+change_mac(options.interface, options.new_mac)
+
+#check MAC changed successfully
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[-] MAC address did not get changed.")
