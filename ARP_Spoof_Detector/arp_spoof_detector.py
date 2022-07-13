3e61b6b41813bd99fa549a1856e71dfba3ca5669
Sheldon Pang <SheldonLinkedin@yahoo.com>
2022-07-12 18:55:25 -0700

    arp spoofing detector v.1.0

---
+++ ARP_Spoof_Detector/arp_spoof_detector.py
@@ -0,0 +1,38 @@
+#!/user/bin/env python
+
+import scapy.all as scapy
+
+
+def get_mac(ip_addr):
+    arp_request = scapy.ARP(pdst=ip_addr)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    # scapy.ls(scapy.ARP())
+
+    srp_answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    # print(srp_answered_list.summary())
+
+    return srp_answered_list[0][1].hwsrc
+
+
+def sniff(interface):
+    scapy.sniff(
+        iface=interface,
+        store=False,
+        prn=process_sniffed_packet
+    )
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(scapy.ARP) and packet[scapy.ARP].op == 2:
+        try:
+            real_mac = get_mac(packet[scapy.ARP].psrc)
+            response_mac = packet[scapy.ARP].hwsrc
+
+            if real_mac != response_mac:
+                print("[+] ARP spoofing detected, possible network attack!")
+        except IndexError:
+            pass
+
+
+sniff("en0")
